a72df56c4fd4e457280e20a539767b8c2749249e
yongzpp <ziping97@gmail.com>
2023-01-08 07:32:37 +0000

    gender-v1

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 #   cd "$(dirname "$0")"
 # fi
 
-# if [ "${LOCAL_IMAGE_NAME}" == "" ]; then 
+# if [ "${LOCAL_IMAGE_NAME}" == "" ]; then
 #     LOCAL_TAG=`date +"%Y-%m-%d-%H-%M"`
 #     export LOCAL_IMAGE_NAME="stream-model-duration:${LOCAL_TAG}"
 #     echo "LOCAL_IMAGE_NAME is not set, building a new image with tag ${LOCAL_IMAGE_NAME}"
@@ -17,7 +17,7 @@ sudo docker-compose up -d
 
 sleep 5
 
-python ./tests/flask_test.py
+python ./tests/integration_test.py
 
 ERROR_CODE=$?
 
