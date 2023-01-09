03c1804d65affa857afae9e1b4a6c8bd33744e0e
yongzpp <ziping97@gmail.com>
2023-01-09 07:07:42 +0000

    add mongo

--- run.sh
+++ run.sh
@@ -17,6 +17,12 @@ sudo docker-compose up -d
 
 sleep 5
 
+sudo apt install -y software-properties-common gnupg apt-transport-https ca-certificates
+sudo apt install -y mongodb
+wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+sudo apt update
+sudo apt install -y mongodb-org
 sudo systemctl start mongod
 
 python ./tests/integration_test.py
