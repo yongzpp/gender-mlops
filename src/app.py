c6223683ccbf5afced87ff22e96e14658f2dff6f
yongzpp <ziping97@gmail.com>
2023-01-13 17:38:50 +0000

    add kube

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ import os
 import mlflow
 import pandas as pd
 import requests
-#from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request
 from pymongo import MongoClient
 
 import bentoml
@@ -24,57 +24,57 @@ mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
 EVIDENTLY_SERVICE_ADDRESS = os.getenv('EVIDENTLY_SERVICE', 'http://127.0.0.1:5000')
 MONGODB_ADDRESS = os.getenv("MONGODB_ADDRESS", "mongodb://127.0.0.1:27017")
 
-# mongo_client = MongoClient(MONGODB_ADDRESS)
-# db = mongo_client.get_database("prediction_service")
-# collection = db.get_collection("data")
+mongo_client = MongoClient(MONGODB_ADDRESS)
+db = mongo_client.get_database("prediction_service")
+collection = db.get_collection("data")
 
-#app = Flask(__name__)
+app = Flask(__name__)
 
-class GenderApplication(BaseModel):
-    name: str
-    gender: str
-    numerical: int
+# class GenderApplication(BaseModel):
+#     name: str
+#     gender: str
+#     numerical: int
 
-model_wrapper = ModelService(RUN_ID)
-bentoml.sklearn.save_model('gender-model', model_wrapper.sklearn_model,
-                            signatures={
-                               'predict': {
-                                   'batchable': True,
-                                   'batch_dim': 0
-                               }
-                           })
-model_ref = bentoml.sklearn.get('gender-model:latest')
-model_runner = model_ref.to_runner()
-svc = bentoml.Service('gender-classifier', runners=[model_runner])
+# model_wrapper = ModelService(RUN_ID)
+# bentoml.sklearn.save_model('gender-model', model_wrapper.sklearn_model,
+#                             signatures={
+#                                'predict': {
+#                                    'batchable': True,
+#                                    'batch_dim': 0
+#                                }
+#                            })
+# model_ref = bentoml.sklearn.get('gender-model:latest')
+# model_runner = model_ref.to_runner()
+# svc = bentoml.Service('gender-classifier', runners=[model_runner])
 
 
-@svc.api(input=JSON(pydantic_model=GenderApplication), output=JSON(), route="/predict")
-async def predict_endpoint(application_data):
-    application_data = application_data.dict()
-    features = ModelService.prepare_features(pd.Series(application_data["name"]))
-    pred = await model_runner.predict.async_run(features)
-    result = {'name': application_data["name"], 'gender': application_data["gender"], 'model_version': RUN_ID, 'prediction': pred[0]}
-    logging.info("Prediction Done...")
-    #save_to_db(application_data, pred[0])
-    logging.info("Saved to Database...")
-    #send_to_evidently_service(application_data, pred[0])
-    logging.info("Sent to Dashboard...")
-    return result
-
-# @app.route('/predict', methods=['POST'])
-# def predict_endpoint():
-#     name = request.get_json()
-#     features = ModelService.prepare_features(pd.Series(name["name"]))
-
-#     model = ModelService(RUN_ID)
-#     pred = model.predict(features)[0]
-#     result = {'name': name["name"], 'gender': pred, 'model_version': RUN_ID}
+# @svc.api(input=JSON(pydantic_model=GenderApplication), output=JSON(), route="/predict")
+# async def predict_endpoint(application_data):
+#     application_data = application_data.dict()
+#     features = ModelService.prepare_features(pd.Series(application_data["name"]))
+#     pred = await model_runner.predict.async_run(features)
+#     result = {'name': application_data["name"], 'gender': application_data["gender"], 'model_version': RUN_ID, 'prediction': pred[0]}
 #     logging.info("Prediction Done...")
-#     save_to_db(name, pred)
+#     #save_to_db(application_data, pred[0])
 #     logging.info("Saved to Database...")
-#     send_to_evidently_service(name, pred)
+#     #send_to_evidently_service(application_data, pred[0])
 #     logging.info("Sent to Dashboard...")
-#     return jsonify(result)
+#     return result
+
+@app.route('/predict', methods=['POST'])
+def predict_endpoint():
+    name = request.get_json()
+    features = ModelService.prepare_features(pd.Series(name["name"]))
+
+    model = ModelService(RUN_ID)
+    pred = model.predict(features)[0]
+    result = {'name': name["name"], 'gender': pred, 'model_version': RUN_ID}
+    logging.info("Prediction Done...")
+    save_to_db(name, pred)
+    logging.info("Saved to Database...")
+    send_to_evidently_service(name, pred)
+    logging.info("Sent to Dashboard...")
+    return jsonify(result)
 
 
 def save_to_db(record, prediction):
@@ -96,5 +96,5 @@ def fetch_data():
     return data_db
 
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
